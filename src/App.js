ea5873b79d18c66e11b8bef060386d7fbada482f
kennieluv <kenny.kim@coxspace.com>
2022-07-27 18:32:18 +0900

    making main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './page/Home.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-headerTop">
+        <text>포커 손나 잘치는법</text>
       </header>
+      <header className="App-header">
+        <text className="Header-text">World Best Poker 2</text>
+      </header>
+      <body className="App-body">
+        <Home />
+      </body>
     </div>
   );
 }
---
+++ src/page/Home.js
@@ -0,0 +1,17 @@
+import './Home.css'
+
+function Home() {
+  return(
+    <div className='Home'>
+      <div className='Home-half'>
+        <text className='Title-text'>Learn Poker from the Best</text>
+        <text className='Body-text'>Bring your game to the next level and join our highly-engaged community supported by some of the world's best poker players</text>
+      </div>
+      <div className='Home-half'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Home;
